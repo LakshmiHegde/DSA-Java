14a8dc5c181876897f029062b19c549c6c430df7
LakshmiHegde <lakshmi.hegde777@gmail.com>
2021-08-25 22:23:25 +0530

    changes committed

---
+++ Trees/Sum_Tree/IsSumTreeCheck.java
@@ -0,0 +1,9 @@
+/*
+To check, if binary tree is sum tree
+Time = O(n), for processing each node
+Space = O(height), for recursion stack
+ */
+package RBR_DSA.Trees.Sum_Tree;
+
+public class IsSumTreeCheck {
+}
---
+++ Trees/Sum_Tree/SumTreeConvert.java
@@ -0,0 +1,4 @@
+package RBR_DSA.Trees.Sum_Tree;
+
+public class SumTreeConvert {
+}
